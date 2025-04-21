f367dd43337ff5533cb1f0a017635ff10d7d2f9d
Alin Prundeanu <prundeanualin@gmail.com>
2025-04-22 01:22:28 +0200

    Unzip data

--- get_data.sh
+++ get_data.sh
@@ -7,19 +7,17 @@ set -euo pipefail
 
 DATA_URL="https://plus.figshare.com/ndownloader/articles/18470912/versions/4"
 TOP_ZIP="things_eeg2.zip"
-TARGET_DIR="Things-EEG2"
 
-# 1. Download
-mkdir -p "$TARGET_DIR"
-cd "$TARGET_DIR"
-echo "Downloading dataset archive ..."
-wget --content-disposition -O "$TOP_ZIP" "$DATA_URL"
+# # 1. Download
+# echo "Downloading dataset archive ..."
+# wget --content-disposition -O "$TOP_ZIP" "$DATA_URL"
 
-# 2. Unpack top‑level downloaded archive
-echo "Unzipping top‑level archive ..."
-unzip -q "$TOP_ZIP"
-rm "$TOP_ZIP"                      
+# # 2. Unpack top‑level downloaded archive
+# echo "Unzipping top‑level archive ..."
+# unzip -q "$TOP_ZIP"
+# rm "$TOP_ZIP"                      
 
+export UNZIP_DISABLE_ZIPBOMB_DETECTION=TRUE # disable zipbomb detection
 # 3. Recursively find every *.zip and expand it next to itself
 echo "Expanding inner zip files ..."
 find . -type f -name '*.zip' -print0 | while IFS= read -r -d '' ZIPFILE; do
