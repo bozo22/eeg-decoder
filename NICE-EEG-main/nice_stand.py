478802311b476cb11e73d1e781f07adf65b673a3
Owen de Jong <O.K.dejong@student.tudelft.nl>
2025-04-26 20:34:43 +0200

    Fix mySequential

--- NICE-EEG-main/nice_stand.py
+++ NICE-EEG-main/nice_stand.py
@@ -199,7 +199,7 @@ class CrossAttention(nn.Module):
         attention_blocks = []
         for i in range(n_blocks):
             attention_blocks.append(CrossAttentionBlock(emb_dim, num_heads, dropout_p))
-        self.attention_blocks = mySequential(attention_blocks)
+        self.attention_blocks = mySequential(*attention_blocks)
 
         # Somewhat arbitrary architecture
         self.mlp = nn.Sequential(
